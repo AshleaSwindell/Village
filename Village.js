1c0ea91177f11aa44fb751fe8c85ea5a943ff702
Ashlea Swindell <ashleaswindell@yahoo.com>
2018-06-02 14:22:09 -0500

    adding an image slider

--- Village.js
+++ Village.js
@@ -3,3 +3,29 @@ $( document ).ready( function(){
         $( "nav ul" ).toggleClass( "open" );
     } );
 } );
+
+
+var i = 0; // start point
+var images = [];
+var time = 4000;
+
+// image list
+images[0] = "image1.jpg";
+images[1] = "image2.jpg";
+images[2] = "image3.jpg";
+images[3] = "image4.jpg";
+
+// change image
+function changeImg(){
+    document.slide.src = images[i];
+
+    if( i < images.length - 1 ){
+        i++;
+    }
+    else{
+        i = 0;
+    }
+    setTimeout( "changeImg()", time );
+}
+
+window.onload = changeImg;
